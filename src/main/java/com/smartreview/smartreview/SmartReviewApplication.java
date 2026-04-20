75485ed3e1318c8bac782a90b5072f8486741e3e
Stefan-Kolov <kolov723@gmail.com>
2026-04-20 12:32:32 +0200

    Initial commit

---
+++ src/main/java/com/smartreview/smartreview/SmartReviewApplication.java
@@ -0,0 +1,13 @@
+package com.smartreview.smartreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartReviewApplication.class, args);
+    }
+
+}
